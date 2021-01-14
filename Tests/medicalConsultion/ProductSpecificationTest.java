83efa81349e20d8737c37f9f5af3397377ddb39e
joanss2 <64496738+joanss2@users.noreply.github.com>
2021-01-14 12:13:59 +0100

    PRODUCTID TEST DONE without HASHCODE

--- Tests/medicalConsultion/ProductSpecificationTest.java
+++ Tests/medicalConsultion/ProductSpecificationTest.java
@@ -1,36 +1,7 @@
 package medicalConsultion;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductSpecificationTest {
 
-    @Test
-    void comprovar_upc() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void setPrice() {
-    }
-
-    @Test
-    void setDescription() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void getPrice() {
-    }
-
-    @Test
-    void getDescription() {
-    }
 }
